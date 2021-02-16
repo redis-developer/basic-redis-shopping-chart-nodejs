abb6031d9021850c7163eb7a4a412dc13f11d59f
adrian-larysz-rmtcfm-com <67647367+adrian-larysz-rmtcfm-com@users.noreply.github.com>
2021-02-16 12:01:23 +0100

    Improvements README and how it works (#15)

--- README.md
+++ README.md
@@ -7,19 +7,31 @@ This shopping cart is using Redis and RedisJson module functionalities, allowing
 ## How it works
 
 ### How the data is stored:
-* The products data is stored in external json file. After first request this data is saved in a JSON data type in Redis like: `JSON.SET product:{productId} . {product data in json format}`.
+* The products data is stored in external json file. After first request this data is saved in a JSON data type in Redis like: `JSON.SET product:{productId} . '{ "id": "productId", "name": "Product Name", "price": "375.00", "stock": 10 }'`.
+    * E.g `JSON.SET product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 . '{ "id": "e182115a-63d2-42ce-8fe0-5f696ecdfba6", "name": "Brilliant Watch", "price": "250.00", "stock": 2 }'`
 * The cart data is stored in a hash like: `HSET cart:{cartId} product:{productId} {productQuantity}`, where cartId is random generated value and stored in user session.
+    * E.g `HSET cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 1`
 
 ### How the data is modified:
-* The product data is modified like `JSON.SET product:{productId} . {new product data in json format}`.
+* The product data is modified like `JSON.SET product:{productId} . '{ "id": "productId", "name": "Product Name", "price": "375.00", "stock": {newStock} }'`.
+    * E.g `JSON.SET product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 . '{ "id": "e182115a-63d2-42ce-8fe0-5f696ecdfba6", "name": "Brilliant Watch", "price": "250.00", "stock": 1 }'`
 * The cart data is modified like `HSET cart:{cartId} product:{productId} {newProductQuantity}` or `HINCRBY cart:{cartId} product:{productId} {incrementBy}`.
+    * E.g `HSET cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 2`
+    * E.g `HINCRBY cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 1`
+    * E.g `HINCRBY cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 -1`
 * Product can be removed from cart like `HDEL cart:{cartId} product:{productId}`
-* Cart can be cleared using `HGETALL cart:{cartId}` and then `HDEL cart:{cartId} {productKey}`.
-* All carts can be deleted when reset data is requested like: `DEL cart:{cartId}`.
+    * E.g `HDEL cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6`
+* Cart can be cleared using `HGETALL cart:{cartId}` and then `HDEL cart:{cartId} {productKey}` in loop.
+    * E.g `HGETALL cart:77f7fc881edc2f558e683a230eac217d` => `product:e182115a-63d2-42ce-8fe0-5f696ecdfba6`, `product:f9a6d214-1c38-47ab-a61c-c99a59438b12`, `product:1f1321bb-0542-45d0-9601-2a3d007d5842` => `HDEL cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6`, `HDEL cart:77f7fc881edc2f558e683a230eac217d product:f9a6d214-1c38-47ab-a61c-c99a59438b12`, `HDEL cart:77f7fc881edc2f558e683a230eac217d product:1f1321bb-0542-45d0-9601-2a3d007d5842`
+* All carts can be deleted when reset data is requested like: `SCAN {cursor} MATCH cart:*` and then `DEL cart:{cartId}` in loop.
+    * E.g `SCAN {cursor} MATCH cart:*` => `cart:77f7fc881edc2f558e683a230eac217d`, `cart:217dedc2f558e683a230eac77f7fc881`, `cart:1ede77f558683a230eac7fc88217dc2f` => `DEL cart:77f7fc881edc2f558e683a230eac217d`, `DEL cart:217dedc2f558e683a230eac77f7fc881`, `DEL cart:1ede77f558683a230eac7fc88217dc2f` 
 
 ### How the data is accessed:
-* Products: `SCAN {cursor} MATCH product:*` to get all product keys and then `JSON.GET {productKey}`.
-* Cart: `HGETALL cart:{cartId}`to get quantity of products and `JSON.GET product:{productId}` to get products data.
+* Products: `SCAN {cursor} MATCH product:*` to get all product keys and then `JSON.GET {productKey}` in loop.
+    * E.g `SCAN {cursor} MATCH product:*` => `product:e182115a-63d2-42ce-8fe0-5f696ecdfba6`, `product:f9a6d214-1c38-47ab-a61c-c99a59438b12`, `product:1f1321bb-0542-45d0-9601-2a3d007d5842` => `JSON.GET product:e182115a-63d2-42ce-8fe0-5f696ecdfba6`, `JSON.GET product:f9a6d214-1c38-47ab-a61c-c99a59438b1`, `JSON.GET product:1f1321bb-0542-45d0-9601-2a3d007d5842`
+* Cart: `HGETALL cart:{cartId}`to get quantity of products and `JSON.GET product:{productId}` to get products data in loop.
+    * E.g `HGETALL cart:77f7fc881edc2f558e683a230eac217d`  => `product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 (quantity: 1)`, `product:f9a6d214-1c38-47ab-a61c-c99a59438b12 (quantity: 0)`, `product:1f1321bb-0542-45d0-9601-2a3d007d5842 (quantity: 2)` => `JSON.GET product:e182115a-63d2-42ce-8fe0-5f696ecdfba6`, `JSON.GET product:f9a6d214-1c38-47ab-a61c-c99a59438b12`, `JSON.GET product:1f1321bb-0542-45d0-9601-2a3d007d5842`
+    * HGETALL returns array of keys and corresponding values from hash data type.
 
 ## Hot to run it locally?
 
--- client/src/components/Info.vue
+++ client/src/components/Info.vue
@@ -36,23 +36,52 @@
             <ol>
                 <li>How the data is stored:</li>
                 <ul class="mb-5">
-                    <li>The products data is stored in external json file. After first request this data is saved in a JSON data type in Redis like: <code>JSON.SET product:{productId} . {product data in json format}</code>.</li>
+                    <li>The products data is stored in external json file. After first request this data is saved in a JSON data type in Redis like: <code>JSON.SET product:{productId} . JSON.SET product:{productId} . '{ "id": "productId", "name": "Product Name", "price": "375.00", "stock": 10 }'</code>.</li>
+                    <ul class="mb-5">
+                        <li>E.g <code>JSON.SET product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 . '{ "id": "e182115a-63d2-42ce-8fe0-5f696ecdfba6", "name": "Brilliant Watch", "price": "250.00", "stock": 2 }'</code></li>
+                    </ul>
                     <li>The cart data is stored in a hash like: <code>HSET cart:{cartId} product:{productId} {productQuantity}</code>, where cartId is random generated value and stored in user session.</li>
+                    <ul class="mb-5">
+                        <li>E.g <code>HSET cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 1</code></li>
+                    </ul>
                 </ul>
 
                 <li>How the data is modified:</li>
                 <ul class="mb-5">
-                    <li>The product data is modified like <code>JSON.SET product:{productId} . {new product data in json format}</code>.</li>
+                    <li>The product data is modified like <code>JSON.SET product:{productId} . '{ "id": "productId", "name": "Product Name", "price": "375.00", "stock": {newStock} }'</code>.</li>
+                    <ul class="mb-5">
+                        <li>E.g <code>JSON.SET product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 . '{ "id": "e182115a-63d2-42ce-8fe0-5f696ecdfba6", "name": "Brilliant Watch", "price": "250.00", "stock": 1 }</code></li>
+                    </ul>
                     <li>The cart data is modified like <code>HSET cart:{cartId} product:{productId} {newProductQuantity}</code> or <code>HINCRBY cart:{cartId} product:{productId} {incrementBy}</code>.</li>
+                    <ul class="mb-5">
+                        <li>E.g <code>HSET cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 2</code></li>
+                        <li>E.g <code>HINCRBY cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 1</code></li>
+                        <li>E.g <code>HINCRBY cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 -1</code></li>
+                    </ul>
                     <li>Product can be removed from cart like <code>HDEL cart:{cartId} product:{productId}</code>.</li>
-                    <li>Cart can be cleared using <code>HGETALL cart:{cartId}</code> and then <code>HDEL cart:{cartId} {productKey}</code>.</li>
-                    <li>All carts can be deleted when reset data is requested like: <code>DEL cart:{cartId}</code></li>
+                    <ul class="mb-5">
+                        <li>E.g <code>HDEL cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6</code></li>
+                    </ul>
+                    <li>Cart can be cleared using <code>HGETALL cart:{cartId}</code> and then <code>HDEL cart:{cartId} {productKey}</code> in loop.</li>
+                    <ul class="mb-5">
+                        <li>E.g <code>HGETALL cart:77f7fc881edc2f558e683a230eac217d</code> => <code>product:e182115a-63d2-42ce-8fe0-5f696ecdfba6</code>, <code>product:f9a6d214-1c38-47ab-a61c-c99a59438b12</code>, <code>product:1f1321bb-0542-45d0-9601-2a3d007d5842</code> => <code>HDEL cart:77f7fc881edc2f558e683a230eac217d product:e182115a-63d2-42ce-8fe0-5f696ecdfba6</code>, <code>HDEL cart:77f7fc881edc2f558e683a230eac217d product:f9a6d214-1c38-47ab-a61c-c99a59438b12</code>, <code>HDEL cart:77f7fc881edc2f558e683a230eac217d product:1f1321bb-0542-45d0-9601-2a3d007d5842</code></li>
+                    </ul>
+                    <li>All carts can be deleted when reset data is requested like: <code>SCAN {cursor} MATCH cart:*</code> and then <code>DEL cart:{cartId}</code> in loop.</li>
+                    <ul class="mb-5">
+                        <li>E.g <code>SCAN {cursor} MATCH cart:*</code> => <code>cart:77f7fc881edc2f558e683a230eac217d</code>, <code>cart:217dedc2f558e683a230eac77f7fc881</code>, <code>cart:1ede77f558683a230eac7fc88217dc2f</code> => <code>DEL cart:77f7fc881edc2f558e683a230eac217d</code>, <code>DEL cart:217dedc2f558e683a230eac77f7fc881</code>, <code>DEL cart:1ede77f558683a230eac7fc88217dc2f</code></li>
+                    </ul>
                 </ul>
 
                 <li>How the data is accessed:</li>
                 <ul class="mb-5">
-                    <li>Products: <code>SCAN {cursor} MATCH product:*</code> to get all product keys and then <code>JSON.GET {productKey}</code></li>
-                    <li>Cart: <code>HGETALL cart:{cartId}</code> to get quantity of products and <code>JSON.GET product:{productId}</code> to get products data.</li>
+                    <li>Products: <code>SCAN {cursor} MATCH product:*</code> to get all product keys and then <code>JSON.GET {productKey}</code> in loop.</li>
+                    <ul class="mb-5">
+                        <li>E.g <code>SCAN {cursor} MATCH product:*</code> => <code>product:e182115a-63d2-42ce-8fe0-5f696ecdfba6</code>, <code>product:f9a6d214-1c38-47ab-a61c-c99a59438b12</code>, <code>product:1f1321bb-0542-45d0-9601-2a3d007d5842</code> => <code>JSON.GET product:e182115a-63d2-42ce-8fe0-5f696ecdfba6</code>, <code>JSON.GET product:f9a6d214-1c38-47ab-a61c-c99a59438b1</code>, <code>JSON.GET product:1f1321bb-0542-45d0-9601-2a3d007d5842</code></li>
+                    </ul>
+                    <li>Cart: <code>HGETALL cart:{cartId}</code> to get quantity of products and <code>JSON.GET product:{productId}</code> to get products data in loop.</li>
+                    <ul class="mb-5">
+                        <li>E.g <code>HGETALL cart:77f7fc881edc2f558e683a230eac217d</code>  => <code>product:e182115a-63d2-42ce-8fe0-5f696ecdfba6 (quantity: 1)</code>, <code>product:f9a6d214-1c38-47ab-a61c-c99a59438b12 (quantity: 0)</code>, <code>product:1f1321bb-0542-45d0-9601-2a3d007d5842 (quantity: 2)</code> => <code>JSON.GET product:e182115a-63d2-42ce-8fe0-5f696ecdfba6</code>, <code>JSON.GET product:f9a6d214-1c38-47ab-a61c-c99a59438b12</code>, <code>JSON.GET product:1f1321bb-0542-45d0-9601-2a3d007d5842</code></li>
+                    </ul>
                 </ul>
             </ol>
         </div>
